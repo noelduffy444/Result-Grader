00bc7ca80f097032fad8db81c52199c7c6508be6
noelduffy444 <35809400+noelduffy444@users.noreply.github.com>
2018-04-19 16:55:50 +0100

    Commit 2

--- g00347796/js/grader.js
+++ g00347796/js/grader.js
@@ -28,4 +28,49 @@ else {
 	document.getElementById("resultsentence").innerHTML = message;
 	}
 
+if(!myIrishResult){
+	alert("Please enter your Irish result");
+}
+else {
+	if (myIrishResult > 90) {
+		irishGrade = 'A';
+	} else if (myIrishResult > 80) {
+		irishGrade = 'B';
+	} else if (myIrishResult >70) {
+		irishGrade = 'B-';
+	} else if (myIrishResult > 60) {
+		irishGrade = 'C';
+	} else if (myIrishResult > 40) {
+		irishGrade = 'D';
+	} else {
+		irishGrade = 'F';
+	}
+	
+	var message = 'Your Irish result of ' + myIrishResult + ' gets you a grade of ' + irishGrade;
+	console.log(message);
+	document.getElementById("resultsentence").innerHTML = message;
+	}
+
+	if(!myMathsResult){
+	alert("Please enter your Maths result");
+}
+else {
+	if (myMathsResult > 90) {
+		mathsGrade = 'A';
+	} else if (myMathsResult > 80) {
+		mathsGrade = 'B';
+	} else if (myMathsResult >70) {
+		mathsGrade = 'B-';
+	} else if (myMathsResult > 60) {
+		mathsGrade = 'C';
+	} else if (myMathsResult > 40) {
+		mathsGrade = 'D';
+	} else {
+		mathsGrade = 'F';
+	}
+	
+	var message = 'Your Maths result of ' + myMathsResult + ' gets you a grade of ' + mathsGrade;
+	console.log(message);
+	document.getElementById("resultsentence").innerHTML = message;
+	}
 }//End of function
